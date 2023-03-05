6563b8a711d16abadc149390eab6e2bccc9af936
saiderrahmani <didi.mousedidi@outlook.com>
2023-03-04 22:52:51 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(num1, num2) {
+    return num1 + num2;
+  }
+  function subtract(num1, num2) {
+    return num1 - num2;
+  }
+  function multiply(num1, num2) {
+    return num1 * num2;
+  }
+  function decrement(num) {
+    return num - 1;
+  }
+  
+  function makeInt(str) {
+    return parseInt(str, 10);
+  }
+  function preserveDecimal(str) {
+    return parseFloat(str);
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(n) {
+    return ++n;
+  }
+  
